51e19fc273b121ff16dc6aa8acfc2db008532d46
Matthias Osswald <m@osswald.li>
2018-11-28 16:40:40 +0100

    Add entry point and argparser
    To be able to convert files from the cli I need to get the output
    directory where files will be saved/created and the paths to files
    which shall be converted

--- pdf_converter/command_line.py
+++ pdf_converter/command_line.py
@@ -1,6 +1,18 @@
+import argparse
+
+
 def main():
-    print("This is gonna be a file to PDF converter.")
-
-
-if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output_dir",
+                        metavar='output-directory',
+                        type=str,
+                        nargs=1,
+                        help="Path to save the file/s in")
+    parser.add_argument("file_paths",
+                        metavar='input-file',
+                        type=str,
+                        nargs='+',
+                        help="Path of file to convert")
+    args = parser.parse_args()
+    print(args.output_dir)
+    print(args.file_paths)
