d4d84861a2ae6a700103d72a938266bb73af0480
Matthias Osswald <m@osswald.li>
2019-08-31 10:59:44 +0200

    Add classifiers for all python versions tested.

--- setup.py
+++ setup.py
@@ -33,6 +33,9 @@ setuptools.setup(
     url=URL,
     packages=setuptools.find_packages(),
     classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU GPL V3",
         "Operating System :: OS Independent",
